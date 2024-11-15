2ec7456158cfba6fa821b81998d13f1dc7fb6c2e
Kirpal <makanga.kirpal@gmail.com>
2024-11-15 02:57:38 +0100

    updated nuxt, removed config hack

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,14 +5,5 @@ export default defineNuxtConfig({
         dirs: ['composables/**']
     },
     css: ['~/assets/style/main.scss'],
-    hooks: {
-        // TODO: remove after next update
-        'vite:extend'({ config }) {
-            if (config.server && config.server.hmr) {
-                //@ts-ignore
-                config.server.hmr.protocol = 'wss';
-            }
-        }
-    },
     compatibilityDate: '2024-10-30'
 });
