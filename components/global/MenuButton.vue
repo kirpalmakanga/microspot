77c2e153f8b32a2f71f0ab59feb6be6018495f07
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-04-12 19:11:03 +0200

    components: added MenuButton

---
+++ components/global/MenuButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { DropdownMenuItem } from '@nuxt/ui';
+
+defineProps<{ menuOptions: DropdownMenuItem[] }>();
+
+const menuConfig = {
+    align: 'start',
+    side: 'bottom'
+};
+</script>
+
+<template>
+    <UDropdownMenu :items="menuOptions" :content="menuConfig">
+        <button
+            class="hover:scale-110 hover:active:scale-90 transition-transform transform cursor-pointer"
+        >
+            <UIcon class="size-8" name="i-mi-options-horizontal" />
+        </button>
+    </UDropdownMenu>
+</template>
