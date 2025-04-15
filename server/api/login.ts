e1d2721a5295ee66fffba088ca7f31800b7630ff
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-04-15 19:38:56 +0200

    server/api/login: handling missing client id

--- server/api/login.ts
+++ server/api/login.ts
@@ -3,6 +3,13 @@ import { CLIENT_ID, REDIRECT_URL, SCOPES } from '~/server/config';
 import { createUrl } from '~/server/helpers';
 
 export default defineEventHandler((event) => {
+    if (!CLIENT_ID) {
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'Environment: CLIENT_ID is not defined'
+        });
+    }
+
     return {
         url: createUrl('https://accounts.spotify.com/authorize', {
             response_type: 'code',
