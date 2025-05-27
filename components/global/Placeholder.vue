dc9dabf566286fe2e28d5396f37d34108b63f2b9
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-27 14:11:38 +0200

    Created error component

--- components/global/Placeholder.vue
+++ components/global/Placeholder.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col grow items-center justify-center gap-2">
+    <div class="flex flex-col grow items-center justify-center gap-2 p-4">
         <UIcon class="size-12" :name="icon" />
 
         <p v-if="text">{{ text }}</p>
