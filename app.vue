320f8841430fe05ed1383711eb9b655e21e52b5e
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-27 14:26:59 +0200

    installed vue-query

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
+const authStore = useAuthStore();
+const { isLoggedIn } = storeToRefs(authStore);
+</script>
+
+<style>
+@import 'tailwindcss';
+@import '@nuxt/ui';
+
+@theme {
+    --font-family-sans: 'Montserrat', 'sans-serif';
+}
+</style>
+
 <template>
     <main
         class="flex flex-col grow min-h-screen bg-zinc-800 overflow-hidden font-sans"
@@ -12,18 +27,5 @@
             <Player v-if="isLoggedIn && $route.name !== 'login'" />
         </UApp>
     </main>
+    <VueQueryDevtools button-position="bottom-left" />
 </template>
-
-<script setup lang="ts">
-const authStore = useAuthStore();
-const { isLoggedIn } = storeToRefs(authStore);
-</script>
-
-<style>
-@import 'tailwindcss';
-@import '@nuxt/ui';
-
-@theme {
-    --font-family-sans: 'Montserrat', 'sans-serif';
-}
-</style>
