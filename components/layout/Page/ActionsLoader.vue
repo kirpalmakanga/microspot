667c229ab6e9fd7c6c9aa7d5ab04e5dc8bf12110
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-30 13:46:52 +0200

    LayoutPageActionsLoader: made play button optional

--- components/layout/Page/ActionsLoader.vue
+++ components/layout/Page/ActionsLoader.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+defineProps<{ hasPlayButton?: boolean }>();
+</script>
+
 <template>
     <div class="flex items-center gap-4 p-4">
-        <USkeleton class="size-16 bg-zinc-600" />
+        <USkeleton v-if="hasPlayButton" class="size-16 bg-zinc-600" />
 
         <USkeleton class="size-8 bg-zinc-600" />
     </div>
