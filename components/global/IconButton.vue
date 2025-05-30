216a426c0fe3e3604e6fc4a0220730a1de9dd9c4
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-30 14:08:58 +0200

    components: added IconButton

---
+++ components/global/IconButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{ icon: string }>();
+</script>
+
+<template>
+    <button
+        class="size-8 transition-transform transform hover:scale-110 hover:active:scale-90 cursor-pointer"
+    >
+        <UIcon class="size-full" :name="icon" />
+    </button>
+</template>
